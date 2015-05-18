89b59b8573f7a7c14bc3eea1f1ee4e5fc303674e
Paul Chechetin <paulche@yandex.ru>
2015-05-18 23:25:29 +0300

    [project 4] First part is ready

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,65 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// *** Pseudo code:
+//    set R2 to 0
+//    loop through R0 to 0 backward and add R1 to R2 each time 
+
+// *** Example:
+// 8 * 5 = 40;
+// R1 | 5 | 4 | 3  | 2  | 1  | 0
+// R2 | 0 | 8 | 16 | 24 | 32 | 40
+
+
+// *** Machine code:
+
+//set R2 to 0
+@R2
+M=0
+
+// Check zero case for either R0 or R1 
+
+//R0
+@R0
+D = M
+
+@END
+D; JEQ
+
+//R1
+@R1
+D = M
+
+@END
+D; JEQ
+
+
+(LOOP)
+  // Decrement R1
+  @R1
+  M = M-1
+
+  //set D to R0
+  @R0
+  D = M
+ 
+  // Add R2 by R0
+  @R2  
+  M = M+D
+  
+  // Set D to R1 to check it 
+  @R1
+  D = M
+
+  // Check loop condition
+  @LOOP
+  D; JGT
+
+  // Exit
+  @END
+  0; JMP
+  
+(END)
+  @END
+  0; JMP
+
