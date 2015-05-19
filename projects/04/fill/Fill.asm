22ac46b7dfd8444ad4172c03e939474b13e9c957
Paul Chechetin <paulche@yandex.ru>
2015-05-19 08:06:52 +0300

    [Project 4] done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,119 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+// Load constants
+// Last address
+@24575
+D = A
+@last_address
+M = D
+
+// Screen state, 0 - filled with white pixels or with black otherwise.
+@state
+M = 0
+
+(MAIN_LOOP)
+  // Pseudo code: 
+  // if (key is pressed)
+  //   if (state is 0) 
+  //     jump GET_BLACK
+  //   end
+  // else 
+  //   if (state is 1)
+  //     jump GET_WHITE 
+  //   end
+  // end
+  //
+  
+  // Probe keyboard
+  @KBD
+  D = M
+
+  @IF_TRUE
+  D; JNE
+
+  @IF_FALSE
+  0; JMP
+
+  (IF_TRUE)
+    @state
+    D = M
+
+    @GET_BLACK
+    D; JEQ
+
+    @MAIN_LOOP
+    0; JMP
+   
+  (IF_FALSE)
+    @state
+    D = M
+
+    @GET_WHITE
+    D; JNE
+
+    @MAIN_LOOP
+    0; JMP
+
+(GET_BLACK)
+  //Load variables
+  @SCREEN
+  D = A
+  @address
+  M = D
+
+  // assert state variable
+  @state
+  M  = 1
+
+  (LOOP0)
+    // Fill next 16 pixels
+    @address
+    A = M
+    M = -1
+
+    @address
+    M = M+1
+
+    D = M
+
+    @last_address
+    D = D - M
+
+    @LOOP0
+    D; JLE
+
+  @MAIN_LOOP
+  0; JMP
+
+(GET_WHITE)
+  //Load variables
+  @SCREEN
+  D = A
+  @address
+  M = D
+
+  // assert state variable
+  @state
+  M  = 0
+
+  (LOOP1)
+    // Fill next 16 pixels
+    @address
+    A = M
+    M = 0
+
+    @address
+    M = M+1
+
+    D = M
+
+    @last_address
+    D = D - M
+
+    @LOOP1
+    D; JLE
+
+  @MAIN_LOOP
+  0; JMP
